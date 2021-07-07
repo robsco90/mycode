432f6eb6efeeb0e519c71cfc2bae5328ce662e2a
robsco90 <scott.robert008@gmail.com>
2021-07-07 15:58:08 +0000

    granda loops

---
+++ grandma_loops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# For every [dish] I find in the [sink] I'm going to [scream].
+
+sink= ["bowl","plate","fork","mug"]
+for dish in sink:
+    print(f"There's a dirty {dish} in the sink! AAAAGGGHH!")
+
+
+room= {"biscuit": flog you, "broccoli": slap you, "dirty dish": whoop your as
+
+    print(f"For every {  } I find in your [room] I'm going to [  ]!")
