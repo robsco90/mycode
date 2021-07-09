62df82e2851e5b5c59a7a1efe5e119bda54ef945
robsco90 <scott.robert008@gmail.com>
2021-07-09 20:14:54 +0000

    Encrypting with vault

---
+++ passingvalues.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+
+
+
+
+isr= {"ship_name": "Bessie", "ship_type": "Intergalactic", "engines": "Dark Matter", "dark_matter_balls": "63", "backup_engines": "Chemical"}
+
+print("Intergalactic Ship Registration:")('/')[-1]
+print("name:"{metadata["ship_name"})
+print("type:"(metadata["ship_type"]))('/')[-1]
+print("\n")
+print("engines:")('/')[-1]
+print("\n")
+print("type:"(metadata[engines["engines"]))
+print("max capacity:"(metadata["dark_matter_balls"]))
+print("backup:"(metadata["backup_engines"]))
